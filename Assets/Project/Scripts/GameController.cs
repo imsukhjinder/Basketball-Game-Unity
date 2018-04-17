5a6ec32ac5455fddffdc033ab7b3f5b3a0fd9e48
Sukhjinder Singh <32549592+imsukhjinder@users.noreply.github.com>
2018-04-17 20:41:56 +0530

    Initial commit

---
+++ Assets/Project/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public Player player;
+	public float resetTimer = 5f;
+    public static int countshots = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(countshots>1){
+			if (player.holdingBall == false) {
+			resetTimer -= Time.deltaTime;
+			
+			if (resetTimer <= 0) {
+				SceneManager.LoadScene ("Game");
+				countshots--;	
+			}
+		}
+		}
+	}
+}
---
+++ Assets/Project/Scripts/Scores.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scores : MonoBehaviour {
+	public Player player;
+	private static int count;
+	public Text countText;
+	public GameObject p;
+	public float rstTimer = 5f;
+	private static int countthrows = 10;
+	public Text countTextthrow;
+
+	// Use this for initialization
+	void Start () {
+		p.SetActive(false);
+		
+	}
+	void OnTriggerEnter (Collider otherCollider) {
+		if (otherCollider.GetComponent<Ball> () != null) {
+			count = count + 5;	
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			countthrows =countthrows - 1;
+
+		}
+		if(countthrows <= 0){
+			rstTimer -= Time.deltaTime;
+			if (rstTimer <= 0) {
+			p.SetActive(true);
+			}
+		}
+		countTextthrow.text = "Throws :" + countthrows.ToString ();	
+		countText.text = "Scores :" + count.ToString ();
+			
+	}
+
+}
